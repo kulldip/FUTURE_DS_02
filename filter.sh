7661e796a0028f0462faf355b4b2b7e1d463c7c7
kulldip <kuldipmhase200@gmail.com>
2026-03-25 02:58:00 +0530

    complete 02

--- filter.sh
+++
@@ -1,8 +0,0 @@
-if [ $GIT_AUTHOR_NAME = 'Aditya Bhambure' ]; then
-  export GIT_AUTHOR_NAME='kulldip'
-  export GIT_AUTHOR_EMAIL='kuldipmhase200@gmail.com'
-fi
-if [ $GIT_COMMITTER_NAME = 'Aditya Bhambure' ]; then
-  export GIT_COMMITTER_NAME='kulldip'
-  export GIT_COMMITTER_EMAIL='kuldipmhase200@gmail.com'
-fi
